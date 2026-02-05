51959d7d795c511f2c50916e9afe9753a2732840
Katrina Moody <kmfoster212@gmail.com>
2026-02-04 20:08:58 -0500

    Working on the hamburger menu some more

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,17 @@
-const styles = {
-  navigation: {
-    width: "100%",
-    height: "50px",
-    backgroundColor: "blue",
-  },
-
-  ul: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "flex-end",
-    margin: "20px 0px",
-    padding: "0 25px",
-  },
-
-  li: {
-    listStyleType: "none",
-    paddingRight: "10px",
-  },
-};
+import Hamburger from "./Menu";
+import "./css/navbar.css";
 
 export default function Navbar() {
   return (
     <>
-      <div className="navigation" style={styles.navigation}>
-        <ul style={styles.ul}>
-          <li style={styles.li}>Home</li>
-          <li style={styles.li}>Projects</li>
-          <li style={styles.li}>Pricing</li>
-          <li style={styles.li}>Contact</li>
+      <div className="navigation">
+        <div className="logo">41st Project</div>
+        <Hamburger />
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Pricing</li>
+          <li>Contact</li>
         </ul>
       </div>
     </>
